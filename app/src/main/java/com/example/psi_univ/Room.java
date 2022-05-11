227fef95567d551e5dace391c40fcc2b82d25d2a
mhamono <morvan.hamono@gmail.com>
2022-05-11 22:48:46 +0200

    Ajout de librairie Ical4j Crétion de Room et dossier pour .ics

---
+++ app/src/main/java/com/example/psi_univ/Room.java
@@ -0,0 +1,15 @@
+package com.example.psi_univ;
+
+public class Room {
+
+    String building;
+    int level;
+    int nb;
+
+    public Room(String building, int level, int nb){
+        this.building = building;
+        this.level = level;
+        this.nb = nb;
+    }
+
+}
