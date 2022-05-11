d7196e322e2b1af7962558b5b1454e4f5c033625
RealHephaistos <issameddine.berrouche@gmail.com>
2022-05-11 23:00:37 +0200

    importer photoview

--- app/src/main/java/com/example/psi_univ/MainActivity.java
+++ app/src/main/java/com/example/psi_univ/MainActivity.java
@@ -3,11 +3,15 @@ package com.example.psi_univ;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.github.chrisbanes.photoview.PhotoView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        PhotoView photoView = (PhotoView) findViewById(R.id.imageView);
+        photoView.setImageResource(R.drawable.univ_rennes_1_plan_beaulieu);
     }
 }
