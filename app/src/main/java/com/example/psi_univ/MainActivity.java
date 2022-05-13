4d023c34220dbcfc8d9b2f8494c49c54d3888150
tbuan <tony.buan@etudiant.univ-rennes1.fr>
2022-05-13 18:31:58 +0200

    Barre d'action couleur de la Fac Barre de Menu supprimer pour avoir uniquement la barre d'action

--- app/src/main/java/com/example/psi_univ/MainActivity.java
+++ app/src/main/java/com/example/psi_univ/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.psi_univ;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mToolBar = findViewById(R.id.ToolBar);
+        getSupportActionBar();
+        setSupportActionBar(mToolBar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.top_app_bar,menu);
+        return true;
     }
 }
