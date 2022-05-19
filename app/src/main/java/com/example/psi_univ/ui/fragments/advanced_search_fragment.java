55a885a8b2344caf202555da0026e9177b9e2470
tbuan <tony.buan@etudiant.univ-rennes1.fr>
2022-05-19 17:46:37 +0200

    Ajout des classes pour les fragments

---
+++ app/src/main/java/com/example/psi_univ/ui/fragments/advanced_search_fragment.java
@@ -0,0 +1,21 @@
+package com.example.psi_univ.ui.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.psi_univ.R;
+
+public class advanced_search_fragment extends  Fragment{
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return getLayoutInflater().inflate(R.layout.fragment_advanced_search,container,false);
+    }
+}
